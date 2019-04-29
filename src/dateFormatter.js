901404dc89477db223fda93f0b6996ed562b4f09
gina chen <ginachen588@gmail.com>
2019-04-29 22:38:09 +0100

    finished exercise

--- src/dateFormatter.js
+++ src/dateFormatter.js
@@ -1,3 +1,22 @@
 export const format = (dateToFormatTimeMillis, systemDateTimeMillis) => {
-  return '';
+  var date = new Date(dateToFormatTimeMillis);
+  var dateCurrent = new Date(systemDateTime);
+
+  if(dateCurrent.getFullYear()!= getFullYear()||
+    dateCurrent.getMonth()!= getMonth()||
+    dateCurrent.getDate()!= getDate()||) {
+
+    var DD = Date.getDate()<10?"0"+ date.getDate():date.getDate();
+    var MM =Date.getMonth()<10?"0"+ date.getMonth():date.getMonth();
+    car YYYY = Date.getFullYear();
+
+    return (DD +"/" + MM +"/"+ ("0000" + YYYY).slice(-4))
+  }
+
+else
+  {
+    return "TODAY";
+
+}
+
 };
